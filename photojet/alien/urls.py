626439f5803afb2746eaac30963ffce9e6b6ab82
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2023-07-22 00:28:57 +0530

    the folder was deleted

--- photojet/alien/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.index),
-    path('TTT',views.TTT,name="TTT")
-]
--- photojet/photo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('/add/', views.add_event, name="add"),
-    path('/events/',views.event_page, name="events"),
-    path('/page/<int:id>', views.page, name="page")
-]
